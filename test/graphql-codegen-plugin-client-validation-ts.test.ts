366d73b5109dce779a385db733ec423309fd452b
Kyle Bradford <kyle@watchtower.ai>
2019-04-30 09:36:00 -0700

    feat: initial setup via typescript-library-starter

---
+++ test/graphql-codegen-plugin-client-validation-ts.test.ts
@@ -0,0 +1,14 @@
+import DummyClass from "../src/graphql-codegen-plugin-client-validation-ts"
+
+/**
+ * Dummy test
+ */
+describe("Dummy test", () => {
+  it("works if true is truthy", () => {
+    expect(true).toBeTruthy()
+  })
+
+  it("DummyClass is instantiable", () => {
+    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  })
+})
