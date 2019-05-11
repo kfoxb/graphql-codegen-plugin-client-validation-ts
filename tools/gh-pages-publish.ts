756e5278316c5db6d488300ca4a469c1c91f77a6
Kyle Bradford <kyle@watchtower.ai>
2019-05-11 14:04:41 -0700

    style: migrate from tslint to eslint

--- tools/gh-pages-publish.ts
+++ tools/gh-pages-publish.ts
@@ -1,31 +1,35 @@
-const { cd, exec, echo, touch } = require("shelljs")
-const { readFileSync } = require("fs")
-const url = require("url")
+/* eslint-disable import/no-extraneous-dependencies */
+import {
+  cd, exec, echo, touch,
+} from 'shelljs';
+import { readFileSync } from 'fs';
+import url from 'url';
 
-let repoUrl
-let pkg = JSON.parse(readFileSync("package.json") as any)
-if (typeof pkg.repository === "object") {
-  if (!pkg.repository.hasOwnProperty("url")) {
-    throw new Error("URL does not exist in repository section")
+let repoUrl;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const pkg = JSON.parse(readFileSync('package.json') as any);
+if (typeof pkg.repository === 'object') {
+  if (!Object.prototype.hasOwnProperty.call(pkg.repository, 'url')) {
+    throw new Error('URL does not exist in repository section');
   }
-  repoUrl = pkg.repository.url
+  repoUrl = pkg.repository.url;
 } else {
-  repoUrl = pkg.repository
+  repoUrl = pkg.repository;
 }
 
-let parsedUrl = url.parse(repoUrl)
-let repository = (parsedUrl.host || "") + (parsedUrl.path || "")
-let ghToken = process.env.GH_TOKEN
+const parsedUrl = url.parse(repoUrl);
+const repository = (parsedUrl.host || '') + (parsedUrl.path || '');
+const ghToken = process.env.GH_TOKEN;
 
-echo("Deploying docs!!!")
-cd("docs")
-touch(".nojekyll")
-exec("git init")
-exec("git add .")
-exec('git config user.name "Kyle Bradford"')
-exec('git config user.email "kyle@watchtower.ai"')
-exec('git commit -m "docs(docs): update gh-pages"')
+echo('Deploying docs!!!');
+cd('docs');
+touch('.nojekyll');
+exec('git init');
+exec('git add .');
+exec('git config user.name "Kyle Bradford"');
+exec('git config user.email "kyle@watchtower.ai"');
+exec('git commit -m "docs(docs): update gh-pages"');
 exec(
-  `git push --force --quiet "https://${ghToken}@${repository}" master:gh-pages`
-)
-echo("Docs deployed!!")
+  `git push --force --quiet "https://${ghToken}@${repository}" master:gh-pages`,
+);
+echo('Docs deployed!!');
