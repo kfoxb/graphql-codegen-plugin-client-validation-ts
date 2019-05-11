47ab67acd6fd0282a138e8967b8839ac8a79575d
Kyle Bradford <kyle@watchtower.ai>
2019-05-11 14:04:41 -0700

    style: migrate from tslint to eslint

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,18 +1,21 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import sourceMaps from 'rollup-plugin-sourcemaps'
-import camelCase from 'lodash.camelcase'
-import typescript from 'rollup-plugin-typescript2'
-import json from 'rollup-plugin-json'
+/* eslint-disable import/no-extraneous-dependencies */
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import sourceMaps from 'rollup-plugin-sourcemaps';
+import camelCase from 'lodash.camelcase';
+import typescript from 'rollup-plugin-typescript2';
+import json from 'rollup-plugin-json';
+import pkg from './package.json';
 
-const pkg = require('./package.json')
+const libraryName = 'graphql-codegen-plugin-client-validation-ts';
 
-const libraryName = 'graphql-codegen-plugin-client-validation-ts'
-
+// eslint-disable-next-line import/no-default-export
 export default {
   input: `src/${libraryName}.ts`,
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
+    {
+      file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true,
+    },
     { file: pkg.module, format: 'es', sourcemap: true },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
@@ -35,4 +38,4 @@ export default {
     // Resolve source maps to the original source
     sourceMaps(),
   ],
-}
+};
