fa8c3d4de649bed0605a11daef1cba33a18ad244
Kyle Bradford <kyle@watchtower.ai>
2019-05-11 18:02:19 -0700

    style: lint test file, generate ts instead of js file

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,18 +1,18 @@
-import gql from 'graphql-tag'
-import { createSchema, plugin } from '../src'
+import gql from 'graphql-tag';
 import { DocumentNode } from 'graphql';
+import { createSchema, plugin } from '../src'
 
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
-const tmpDir = path.resolve(__dirname, "../tmp/")
+const tmpDir = path.resolve(__dirname, '../tmp/');
 
 describe('plugin', () => {
   beforeAll(() => {
     try {
-      fs.mkdirSync(tmpDir)
-    } catch(e) {}
-  })
+      fs.mkdirSync(tmpDir);
+    } catch (e) {}
+  });
   describe('createSchema', () => {
     it('creates a schema with a basic query', () => {
       const input: DocumentNode = gql`
@@ -21,47 +21,47 @@ describe('plugin', () => {
             title @validate(maxLength: 20),
           }
         }
-      `
-      const definitionNode = input.definitions[0]
+      `;
+      const definitionNode = input.definitions[0];
       const expected = {
         not: {
-          type: 'null'
+          type: 'null',
         },
         properties: {
           blog: {
             not: {
-              type: 'null'
+              type: 'null',
             },
             properties: {
               title: {
-                maxLength: 20
-              }
-            }
-          }
-        }
-      }
-      const actual = createSchema(definitionNode, [])
-      expect(actual).toEqual(expected)
-    })
-  })
+                maxLength: 20,
+              },
+            },
+          },
+        },
+      };
+      const actual = createSchema(definitionNode, []);
+      expect(actual).toEqual(expected);
+    });
+  });
 
-  it("creates a validation function for a basic query", () => {
+  it('creates a validation function for a basic query', () => {
     const input: DocumentNode = gql`
       query getBlog @validate(not: { type: "null" }) {
         blog @validate(not: { type: "null" }) {
           title @validate(maxLength: 20),
         }
       }
-    `
+    `;
     const validResult = {
       blog: {
-        title: "example title"
-      }
-    }
-    const generatedCode = plugin(null, [{ filePath: "", content: input }])
-    fs.writeFileSync(tmpDir + "/generated.js", generatedCode)
-    const {validateGetBlog} = require("../tmp/generated.js")
-    const [actual] = validateGetBlog(validResult)
-    expect(!!actual).toEqual(true)
-  })
-})
+        title: 'example title',
+      },
+    };
+    const generatedCode = plugin(null, [{ filePath: '', content: input }]);
+    fs.writeFileSync(`${tmpDir  }/generated.ts`, generatedCode);
+    const { validateGetBlog } = require('../tmp/generated.ts');
+    const [actual] = validateGetBlog(validResult);
+    expect(!!actual).toEqual(true);
+  });
+});
